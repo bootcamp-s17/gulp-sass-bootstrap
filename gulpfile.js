b6b2f1fba7a684249d1cd5cbf1a472bfb413c84d
Janine Hempy <jhempy@gmail.com>
2017-05-18 11:11:45 -0400

    Added default gulp task

---
+++ gulpfile.js
@@ -0,0 +1,11 @@
+var gulp = require('gulp');
+
+// Basic pattern:
+//
+//  gulp.task('task-name', function() {
+//    // Stuff here
+//  });
+
+gulp.task('default', function() {
+  console.log('Hello!');
+});
