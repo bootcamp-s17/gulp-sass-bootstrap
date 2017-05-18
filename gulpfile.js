ffc6de2c7f181f907e41d4906a2a7b303407b3be
Janine Hempy <jhempy@gmail.com>
2017-05-18 14:20:24 -0400

    Added SASS watcher

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,27 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 
+
+
 // Basic pattern:
 //
 //  gulp.task('task-name', function() {
 //    // Stuff here
 //  });
+
+// Applying a plugin to a directory:
+//
+//  gulp.task('task-name', function () {
+//    return gulp.src('source-files') // Get source files with gulp.src
+//      .pipe(aGulpPlugin()) // Sends it through a gulp plugin
+//      .pipe(gulp.dest('destination')) // Outputs the file in the destination folder
+//  })
+
+// Watching a directory:
+//
+//  gulp.watch('files-to-watch', ['tasks', 'to', 'run']); 
+
+
 
 gulp.task('default', function() {
   console.log('Hello!');
@@ -15,4 +31,8 @@ gulp.task('sass', function() {
   return gulp.src('app/scss/**/*.scss')
     .pipe(sass())
     .pipe(gulp.dest('dist/css'))
+});
+
+gulp.task('watch', function(){
+  gulp.watch('app/scss/**/*.scss', ['sass']); 
 });
